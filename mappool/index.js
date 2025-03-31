757cca5f465bf1edda04f16b9dc06845ade85c2e
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-03-31 23:12:59 +1100

    Complete star display

--- mappool/index.js
+++ mappool/index.js
@@ -1,10 +1,61 @@
+// Round Text
+const roundTextEl = document.getElementById("round-text")
+
+// Get beatmaps
+let currentBestOf = 0, currentFirstTo = 0, currentRedScore = 0, currentBlueScore = 0
+let allBeatmaps
+async function getBeatmaps() {
+    const response = await fetch("../_data/beatmaps.json")
+    const responseJson = await response.json()
+    allBeatmaps = responseJson.beatmaps
+    
+    const roundName = responseJson.roundName
+    roundTextEl.innerText = roundName
+    currentBestOf = 13
+    switch (roundName) {
+        case "RO32": case "RO16":
+            currentBestOf = 9
+            break
+        case "QF": case "SF":
+            currentBestOf = 11
+            break
+    }
+    currentFirstTo = Math.ceil(currentBestOf / 2)
+
+    createStarDisplay()
+}
+getBeatmaps()
+
+// Star container
+const redTeamStarContainerEl = document.getElementById("red-team-star-container")
+const blueTeamStarContainerEl = document.getElementById("blue-team-star-container")
+
+// Create star display
+function createStarDisplay() {
+    redTeamStarContainerEl.innerHTML = ""
+    blueTeamStarContainerEl.innerHTML = ""
+
+    let i = 0
+    for (i; i < currentRedScore; i++) redTeamStarContainerEl.append(createStar("fill"))
+    for (i; i < currentFirstTo; i++) redTeamStarContainerEl.append(createStar("empty"))
+
+    i = 0
+    for (i; i < currentBlueScore; i++) blueTeamStarContainerEl.append(createStar("fill"))
+    for (i; i < currentFirstTo; i++) blueTeamStarContainerEl.append(createStar("empty"))
+
+    // Create Star
+    function createStar(status) {
+        const newStar = document.createElement("div")
+        newStar.classList.add("team-star", `team-star-${status}`)
+        return newStar
+    }
+}
+
 // Team Info
 const redTeamIconEl = document.getElementById("red-team-icon")
 const blueTeamIconEl = document.getElementById("blue-team-icon")
 const redTeamNameEl = document.getElementById("red-team-name")
 const blueTeamNameEl = document.getElementById("blue-team-name")
-const redTeamStarContainerEl = document.getElementById("red-team-star-container")
-const blueTeamStarContainerEl = document.getElementById("blue-team-star-container")
 let redTeamName, blueTeamName
 
 const socket = createTosuWsSocket()
