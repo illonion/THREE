642faf5bafd1c246d836d3dde38fcff4fcecf7ba
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-04-03 22:18:20 +1100

    Team name and icons work

---
+++ main/index.js
@@ -0,0 +1,24 @@
+// Team Info
+const redTeamIconEl = document.getElementById("red-team-icon")
+const blueTeamIconEl = document.getElementById("blue-team-icon")
+const redTeamNameEl = document.getElementById("red-team-name")
+const blueTeamNameEl = document.getElementById("blue-team-name")
+let redTeamName, blueTeamName
+
+const socket = createTosuWsSocket()
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    // Team Details 
+    if (redTeamName !== data.tourney.manager.teamName.left) {
+        redTeamName = data.tourney.manager.teamName.left
+        redTeamNameEl.innerText = redTeamName
+        redTeamIconEl.style.backgroundImage = `url("../_shared/assets/team-icons/${redTeamName.replace(/[<>:"/\\|?*]/g, '_')}.png")`
+    }
+    if (blueTeamName !== data.tourney.manager.teamName.right) {
+        blueTeamName = data.tourney.manager.teamName.right
+        blueTeamNameEl.innerText = blueTeamName
+        blueTeamIconEl.style.backgroundImage = `url("../_shared/assets/team-icons/${blueTeamName.replace(/[<>:"/\\|?*]/g, '_')}.png")`
+    }
+}
